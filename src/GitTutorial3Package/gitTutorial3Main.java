91e25f3a4d763255783e2cbcb6134ee22a2ba021
Brandon Stamba <brandonstamba@gmail.com>
2023-07-13 14:49:09 -0500

    Version 2 after Cloning/Importing

--- src/GitTutorial3Package/gitTutorial3Main.java
+++ src/GitTutorial3Package/gitTutorial3Main.java
@@ -6,6 +6,6 @@ public class gitTutorial3Main {
 		
 		gitTutorial3Methods g = new gitTutorial3Methods();
 		g.getMessage();
-		System.out.println("This is Version 1.");
+		System.out.println("This is Version 2 after cloned from GitHub and imported into Eclipse.");
 	}
 }
