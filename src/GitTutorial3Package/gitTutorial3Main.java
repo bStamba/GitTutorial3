101cee47b13bc573064f5cf4fbe076e985892ffd
Brandon Stamba <brandonstamba@gmail.com>
2023-07-13 14:39:42 -0500

    Version 1 GitHub Tutorial

---
+++ src/GitTutorial3Package/gitTutorial3Main.java
@@ -0,0 +1,11 @@
+package GitTutorial3Package;
+
+public class gitTutorial3Main {
+
+	public static void main(String[] args) {
+		
+		gitTutorial3Methods g = new gitTutorial3Methods();
+		g.getMessage();
+		System.out.println("This is Version 1.");
+	}
+}
---
+++ src/GitTutorial3Package/gitTutorial3Methods.java
@@ -0,0 +1,8 @@
+package GitTutorial3Package;
+
+public class gitTutorial3Methods {
+
+	public void getMessage() {
+		System.out.println("This is our third Git tutorial.");
+	}
+}
